b797dd504a65d342e8731c3e6b9f4d8f9bc146ee
coderaiser <coderaiser@cloudcmd.io>
2018-11-28 14:39:57 +0200

    feature(package) sinon -> @cloudcmd/stub

--- test/criton.js
+++ test/criton.js
@@ -1,9 +1,10 @@
 'use strict';
 
-let crypto  = require('crypto');
-let test    = require('tape');
-let sinon   = require('sinon');
-let criton  = require('..');
+const crypto = require('crypto');
+const test = require('tape');
+const stub = require('@cloudcmd/stub');
+
+const criton = require('..');
 
 test('no arguments', (t) => {
     t.throws(criton, /password could not be empty!/, 'should throw when no password');
@@ -11,26 +12,26 @@ test('no arguments', (t) => {
 });
 
 test('arguments: no algo, just a password', (t) => {
-    let hash = '9b71d224bd62f3785d96d46ad3ea3d73319bfbc2890caadae2dff72519673ca72323c3d99ba5c11d7c7acc6e14b8c5da0c4663475c2e5c3adef46f73bcdec043';
+    const hash = '9b71d224bd62f3785d96d46ad3ea3d73319bfbc2890caadae2dff72519673ca72323c3d99ba5c11d7c7acc6e14b8c5da0c4663475c2e5c3adef46f73bcdec043';
     
     t.equal(criton('hello'), hash, 'sha512WithRSAEncryption should be used');
     t.end();
 });
 
 test('arguments: password and algo used', (t) => {
-    let hash = '5d41402abc4b2a76b9719d911017c592';
+    const hash = '5d41402abc4b2a76b9719d911017c592';
     
     t.equal(criton('hello', 'MD5'), hash, 'should crypt with defined algo');
     t.end();
 });
 
 test('internals: crypto should be used', (t) => {
-    let createHash = crypto.createHash;
-    let algo = 'MD5';
+    const createHash = crypto.createHash;
+    const algo = 'MD5';
     
-    crypto.createHash = sinon.stub().returns({
-        update: sinon.stub(),
-        digest: sinon.stub()
+    crypto.createHash = stub().returns({
+        update: stub(),
+        digest: stub()
     });
     
     criton('hello', algo);
@@ -42,14 +43,14 @@ test('internals: crypto should be used', (t) => {
 });
 
 test('internals: hash update should be used', (t) => {
-    let createHash = crypto.createHash;
-    let algo = 'MD5';
-    let update = sinon.spy();
-    let digest = sinon.spy();
+    const createHash = crypto.createHash;
+    const algo = 'MD5';
+    const update = stub();
+    const digest = stub();
     
-    crypto.createHash = sinon.stub().returns({
-        update: update,
-        digest: digest
+    crypto.createHash = stub().returns({
+        update,
+        digest,
     });
     
     criton('hello', algo);
@@ -61,14 +62,14 @@ test('internals: hash update should be used', (t) => {
 });
 
 test('internals: hash update should be used', (t) => {
-    let createHash = crypto.createHash;
-    let algo = 'MD5';
-    let update = sinon.spy();
-    let digest = sinon.spy();
+    const createHash = crypto.createHash;
+    const algo = 'MD5';
+    const update = stub();
+    const digest = stub();
     
-    crypto.createHash = sinon.stub().returns({
-        update: update,
-        digest: digest
+    crypto.createHash = stub().returns({
+        update,
+        digest,
     });
     
     criton('hello', algo);
@@ -78,3 +79,4 @@ test('internals: hash update should be used', (t) => {
     crypto.createHash = createHash;
     t.end();
 });
+
